d0766225eb41748cc3736760fb069e597952277b
Gerhard Benkovsky <benkovskigeri@gmail.com>
2019-10-30 16:22:12 +0100

    made a few changes

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.listen(3000, () => console.log("Listening at 3000"));
-
 app.use(express.static("public"));
 
+app.listen(3000, () => console.log("Listening at 3000"));
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/");
+    res.render('index');
   });
-  
+
 app.get("/:load/",(req,res) => {
   res.render('content', {load: req.params.load});
 });
--- public/picker.js
+++ public/picker.js
@@ -1,14 +1,11 @@
 $(document).ready(function(){
     createSelector();
 });
-
 async function createSelector() {
 
     const url = "https://cdn.glitch.com/88ee8401-866c-4005-b70e-7fed0d9b68ba%2Fdata-test.JSON?v=1572291112969";
     const reponse = await fetch(url);
     const data = await reponse.json();
-
-
 
     $("main").append("<section class='lessonSelector'></section>");
     $(".lessonSelector").append("<h2></h2>");
@@ -21,12 +18,9 @@ async function createSelector() {
     for (let i = 0; i < data.length ; i++) {
         $(".lessonSelector > select").append("<option value = ' " + data[i].id + " '>" + data[i].id +"</option>");
     }
-
     $(".lessonSelector").append("<button id='activator'>Submit</button>");
-
     btnLoader();
 }
-
 
 // End -----------------
 
