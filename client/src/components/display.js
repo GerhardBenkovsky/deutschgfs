711920ad5e5bff363963aa8ab1fe7c134ac567cb
Gerhard Benkovsky <benkovskigeri@gmail.com>
2019-11-01 15:11:37 +0100

    cleaned up a bit,added concurrently

--- client/src/components/display.js
+++ client/src/components/display.js
@@ -1,12 +1,30 @@
 import React from "react";
 import "./display.css";
 
-function Display() {
-  return (
-    <div>
-      <h1>Text</h1>
-    </div>
-  );
+class Display extends React.Component {
+  state = {
+    data: []
+  };
+
+  componentDidMount() {
+    fetch("/getdata/all")
+      .then(res => res.json())
+      .then(data =>
+        this.setState({ data }, () => console.log("data fetched..", data))
+      );
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.data.map(item => (
+            <li key={item.id}> {item.title} </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Display;
