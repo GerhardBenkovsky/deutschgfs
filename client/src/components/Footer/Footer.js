0720c0bc7a3bfed4a2d401a799a44a3baa623be1
Gerhard Benkovsky <benkovskigeri@gmail.com>
2020-02-16 18:13:47 +0100

    changed footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -9,21 +9,25 @@ export default function Footer() {
       {context => (
         <footer>
           <section id="footer-left">
-            This Page was build as part of a Scoolproject
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt
+            distinctio quidem corrupti aperiam odio odit molestiae libero
+            tenetur dolores dolorem, voluptatibus sapiente ducimus
+            necessitatibus quia repudiandae aut rerum repellat adipisci cumque
+            fuga accusamus hic mollitia perferendis officiis. Doloremque minus
+            officiis autem! Exercitationem enim accusamus saepe ipsum, qui eius
+            quia quidem tempora necessitatibus impedit doloribus dignissimos
+            magnam alias, molestias, doloremque sapiente eveniet officia vero ea
+            totam facere non sunt omnis corporis. Id ducimus quo recusandae
+            velit quasi incidunt quam magni maiores minus beatae, quae
+            accusantium iste est reiciendis, suscipit aliquam ex dignissimos
+            voluptate nesciunt. Iusto incidunt distinctio voluptas minus rerum
+            minima.
           </section>
           <section id="footer-right">
             <ul>
-              {context.Navlinks.map(item =>
-                item.name === "About" ? (
-                  ""
-                ) : item.name === "Home" ? (
-                  ""
-                ) : (
-                  <li key={item.name}>
-                    <a href={item.link}>{item.name}</a>
-                  </li>
-                )
-              )}
+              <li>Email: someemail@email.com</li>
+              <li>Tel:01234556789</li>
+              <li>More Contect info</li>
             </ul>
           </section>
         </footer>
