4bda6cf556335e08dd92d03804dba584870d865b
Gerhard Benkovsky <benkovskigeri@gmail.com>
2020-03-07 16:12:04 +0100

    wiktionary implementation

--- client/src/components/StartPage/Duden.js
+++ client/src/components/StartPage/Duden.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Duden extends Component {
+  async search(word) {
+    try {
+      const response = await axios.get(
+        'https://de.wikipedia.org/w/api.php?action=query&prop=revisions&titles=Pizza&rvslots=*&rvprop=content&formatversion=2&format=json'
+      );
+      console.log(response);
+    } catch (e) {
+      console.log(e);
+    }
+
+    return word;
+  }
+
   render() {
     return (
       <div id="duden">
-        <p>Duden</p>
+        <p> gestern</p>
+        {this.search('gestern')}
       </div>
     );
   }
