b4b81c0dbb5b86e2d44aacfacb19d789f2ddeaab
Gerhard Benkovsky <benkovskigeri@gmail.com>
2020-03-07 14:30:48 +0100

    initialized DudenAPI, banching

---
+++ client/src/components/StartPage/Duden.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Duden extends Component {
+  render() {
+    return (
+      <div id="duden">
+        <p>Duden</p>
+      </div>
+    );
+  }
+}
