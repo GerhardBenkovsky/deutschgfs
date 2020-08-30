186a7fe52fb20c30a72e23d6165ebff9a370f9df
Gerhard Benkovsky <benkovskigeri@gmail.com>
2020-08-30 14:58:15 +0200

    final fixes

--- client/src/components/Content/content.js
+++ client/src/components/Content/content.js
@@ -11,44 +11,6 @@ export default function Content() {
       {(context) => {
         const { content } = context;
 
-<<<<<<< HEAD
-        return !contentHasError ? (
-          content.map((item) => {
-            const { id, logo, title, description, links } = item;
-            return id === window.location.pathname.split('/')[2] ? (
-              <React.Fragment>
-                <div className="lesson-description">
-                  <div className="lp-img">
-                    <LinkFrame link={logo} />
-                  </div>
-                  <div className="lesson-description-text">
-                    <h1>{title}</h1>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <hr
-                  style={{
-                    width: '90%',
-                    height: '2px',
-                    background: 'red',
-                    marginTop: '1em',
-                    marginBottom: '1em',
-                    border: 0,
-                  }}
-                ></hr>
-                <div className="Link-Wrapper">
-                  {links.map((item) => {
-                    const { link, text } = item;
-                    return <LinkFrame link={link} text={text} key={text} />;
-                  })}
-                </div>
-              </React.Fragment>
-            ) : null;
-          })
-        ) : (
-          <Error />
-        );
-=======
         return content.map((item) => {
           const { id, logo, title, description, links } = item;
 
@@ -82,7 +44,6 @@ export default function Content() {
             </React.Fragment>
           ) : null;
         });
->>>>>>> clientdev
       }}
     </ContentConsumer>
   );
